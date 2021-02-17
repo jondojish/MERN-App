01d3d596c3069d1873e2e1861738155e99685b00
Jonathan F <jfesseha.imp@gmail.com>
2021-02-17 00:01:30 +0000

    ready for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const auth = require("./routes/api/auth");
@@ -40,6 +41,16 @@ app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/api/messages", messages);
 app.use("/api/profile", imageUpload);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
